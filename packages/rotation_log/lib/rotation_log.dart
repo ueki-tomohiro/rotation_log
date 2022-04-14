c21d41e1c196f2c847f74a550832a001d895dbdd
植木 友浩 <ueki_tomohiro@cyberagent.co.jp>
2022-04-14 12:36:08 +0900

    rotation log

---
+++ packages/rotation_log/lib/rotation_log.dart
@@ -0,0 +1,9 @@
+library rotation_log;
+
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:clock/clock.dart';
+import 'package:path_provider/path_provider.dart';
+
+part 'src/term.dart';
+part 'src/rotation.dart';
---
+++ packages/rotation_log/lib/src/rotation.dart
@@ -0,0 +1,89 @@
+part of rotation_log;
+
+enum RotationLogLevelEnum { info, debug, warning, error }
+
+class RotationLog {
+  RotationLogTerm term;
+  late bool initialized;
+
+  IOSink? _sink;
+  List<String> _lines = [];
+
+  String? _logFileName;
+  String get logFileName => _logFileName ?? "";
+
+  RotationLog(this.term) {
+    initialized = false;
+  }
+
+  Future init() async {
+    final documents = await getApplicationDocumentsDirectory();
+
+    if (term.option == RotationLogTermEnum.line) {
+      final file = File(documents.path + "/rotation.log");
+      _logFileName = file.absolute.path;
+      if (await file.exists()) {
+        final sink = await file.open(mode: FileMode.read);
+        _lines = await file.readAsLines();
+        if (_lines.length > term.line) {
+          _lines = _lines.skip(_lines.length - term.line).toList();
+        }
+        await sink.close();
+      }
+    } else {
+      final match = RegExp(r'^[0-9]+$');
+      await for (var entity
+          in documents.list(recursive: true, followLinks: false)) {
+        if (path.extension(entity.path) == ".log") {
+          final filename = path.basenameWithoutExtension(entity.path);
+          if (match.hasMatch(filename)) {
+            final created =
+                DateTime.fromMicrosecondsSinceEpoch(int.parse(filename));
+            if (term.isNeedRotationFromDateTime(created)) {
+              await File(entity.absolute.path).delete();
+            }
+          }
+        }
+      }
+      final filename = term.createFileName();
+      final file = File(documents.path + "/$filename");
+      _logFileName = file.absolute.path;
+      _sink = file.openWrite(mode: FileMode.writeOnly);
+    }
+    initialized = true;
+  }
+
+  Future log(RotationLogLevelEnum level, String message) async {
+    append("[$level]: $message");
+  }
+
+  Future append(String log) async {
+    if (term.option == RotationLogTermEnum.line) {
+      _lines.add(log);
+    } else {
+      _sink?.writeln(log);
+    }
+  }
+
+  void clear() {
+    _lines = [];
+  }
+
+  Future close() async {
+    if (term.option == RotationLogTermEnum.line) {
+      final documents = await getApplicationDocumentsDirectory();
+      final file = File(documents.path + "/rotation.log");
+      final sink = file.openWrite(mode: FileMode.writeOnly);
+      if (_lines.length > term.line) {
+        _lines = _lines.skip(_lines.length - term.line).toList();
+      }
+      sink.writeAll(_lines, "\n");
+      await sink.flush();
+      await sink.close();
+    } else {
+      await _sink?.flush();
+      await _sink?.close();
+    }
+    clear();
+  }
+}
